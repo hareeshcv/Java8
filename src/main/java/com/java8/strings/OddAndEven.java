96694edc0a500c0ba68c40e7fd3936ca11fb97a6
hareeshcv <chareesh8519@gmail.com>
2025-04-07 09:38:48 +0530

    string code practise on java8

--- src/main/java/com/java8/strings/OddAndEven.java
+++ src/main/java/com/java8/strings/OddAndEven.java
@@ -1,8 +1,12 @@
 package com.java8.strings;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OddAndEven {
 
     public static void main(String[] args){
 
+        List<Integer> list = Arrays.asList(10,23,12,34,65,5,8);
     }
 }
